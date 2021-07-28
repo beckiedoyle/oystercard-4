548d906facc21ca967e3a6d843362e9de316a3bf
NazirShaikh <shain003.212@gmail.com>
2021-07-28 14:10:16 +0100

    Pushed all work to day3 repo

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,10 +1,12 @@
 class Oystercard
-attr_reader :balance
+attr_reader :balance, :MINIMUM_FARE, :entry_station
 
 MAX_LIMIT = 90
 
-  def initialize(balance=0)
+  def initialize(balance=0, minimum_fare=1)
     @balance = balance
+    @MINIMUM_FARE = minimum_fare
+    @entry_station
   end
 
   def top_up(amount)
@@ -12,25 +14,30 @@ MAX_LIMIT = 90
     @balance += amount
   end
 
+  def in_journey?
+    !!@entry_station
+  end
+
+  def touch_in(station)
+    raise ("You need at least #{@MINIMUM_FARE} to touch in") if under_fare?
+    @entry_station = station
+  end
+
+  def touch_out
+    self.deduct(@MINIMUM_FARE)
+    @entry_station = nil
+  end
+
+  private
   def deduct(value)
     @balance -= value
   end
 
-  def in_journey?
-    @in_journey
-  end
-
-  def touch_in
-    @in_journey = true
-  end
-
-  def touch_out
-    @in_journey = false
-  end
-
-  private
   def full?
     @balance >= MAX_LIMIT
   end
 
+  def under_fare?
+    @balance < @MINIMUM_FARE
+  end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,6 +1,8 @@
 require 'oystercard'
 
 describe Oystercard do
+  let(:station){ double :station }
+
   it 'should have a balance of 0' do
     oystercard = Oystercard.new
     expect(oystercard.balance).to eq(0)
@@ -16,25 +18,37 @@ describe Oystercard do
     expect{ subject.top_up 1 }.to raise_error "Reached max limit of £90"
   end
 
-  it 'should deduct spending' do
-    max_limit = Oystercard::MAX_LIMIT
-    subject.top_up(max_limit)
-    expect(subject.deduct 10).to eq(80)
-  end 
-
   it 'is in journey' do
     expect(subject).not_to be_in_journey
   end 
 
   it 'can touch in' do
-    subject.touch_in
+    subject.top_up(30)
+    subject.touch_in(station)
     expect(subject).to be_in_journey
   end
 
   it 'can touch out' do
-    subject.touch_in
+    subject.top_up(30)
+    subject.touch_in(station)
     subject.touch_out
     expect(subject).not_to be_in_journey
   end
 
+  it 'raises an error when user balance is under minimum fare' do
+    card = Oystercard.new
+    expect{card.touch_in(station)}.to raise_error("You need at least #{card.MINIMUM_FARE} to touch in")
+  end
+
+  it 'deducts minimum fare from balance when touching out' do
+    subject.top_up(30)
+    subject.touch_in(station)
+    expect{subject.touch_out}.to change{subject.balance}.by(-subject.MINIMUM_FARE)
+  end
+
+  it 'stores the entry station' do
+    subject.top_up(40)
+    subject.touch_in(station)
+    expect(subject.entry_station).to eq(station)
+  end
 end
